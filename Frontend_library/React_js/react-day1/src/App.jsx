b53b377007c75a88b5de503806d9c4f5a5664df2
coderx-shamsher <shamsher000789@gmail.com>
2026-07-29 17:47:11 +0530

    React src folder with all react code file

---
+++ Frontend_library/React_js/react-day1/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Frontend from './Frontend'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Hello form React Js </h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is -&gt; {count}
+        </button>
+        
+        <div className="mytestingappcode ">
+             <Frontend />
+
+        </div>
+        </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Js and react is good in webdev </h2>
+          <p>you need to learn js first then react js </p>
+        </div>
+
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend_library/React_js/react-day1/src/Frontend.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+const Frontend = () => {
+  return (
+    <div>This is Frontend app code running now </div>
+  )
+}
+
+export default Frontend
