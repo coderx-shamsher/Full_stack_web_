dd4817508fc15d4f61fa53b2ea88a0d3fff7d703
coderx-shamsher <shamsher000789@gmail.com>
2026-08-10 14:51:05 +0530

    app jsx file main component

---
+++ Frontend_library/React_Projects/reactbgchanger/src/App.jsx
@@ -0,0 +1,14 @@
+import Themechanger from "./Components/Themechanger"
+
+function App() {
+  return (
+    <>
+       <div className="">
+          this is testing text
+          <Themechanger/> 
+       </div>
+    </>
+  ) 
+}
+
+export default App
