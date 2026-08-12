8acaec8cfebd185252721db3a3123a07d5a938a0
coderx-shamsher <shamsher000789@gmail.com>
2026-08-12 12:06:10 +0530

    server config kra with promise handling

--- Js_Backend/04_backend_/Backend/src/Server.js
+++ Js_Backend/04_backend_/Backend/src/Server.js
@@ -1,10 +1,20 @@
-// 1) step is to setup dotenv , keoki hamein env variables bhi load krne honge usi time par jab mera server load hoga  and first hame use setup krna hoga 
+// 1) step is to setup dotenv , keoki hamein env variables bhi load krne honge usi time par jab mera server load hoga  and first hame use setup krna hoga
 
-import 'dotenv/config'
+import "dotenv/config";
 
 import ConnectDB from "./db/connection.js";
 
-
-
 // calling function
 ConnectDB()
+    .then(() => {
+        // connect port of app in this with app sever connection success message
+        
+        const Listen_port = process.env.Port || 4080;
+
+        app.listen(Listen_port, () => {
+            console.log(` ♾️  Server is Running at localhost:${Listen_port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("\n Error : MongoDB DB connection failed !!! ‼️ ");
+    });
