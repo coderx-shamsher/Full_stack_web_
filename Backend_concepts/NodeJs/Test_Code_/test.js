f9310922fe0394602f527db1f912ea7126654d31
coderx-shamsher <shamsher000789@gmail.com>
2026-08-20 21:50:19 +0530

    test node code

---
+++ Backend_concepts/NodeJs/Test_Code_/test.js
@@ -0,0 +1,14 @@
+const { model } = require("mongoose");
+
+console.log("this is node js ")
+// console.log()
+// (() => {
+//   console.log("this is function self invoking function");
+// })();
+
+console.log()
+
+// globalThis.console.log(module)
+// console.log(module)  // same without globalthis 
+
+
