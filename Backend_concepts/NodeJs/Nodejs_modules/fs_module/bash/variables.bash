e370120749d4f2402f5a0a76316c64d2a9a60f3c
coderx-shamsher <shamsher000789@gmail.com>
2026-08-21 11:47:53 +0530

    bash testing files created by fs module

---
+++ Backend_concepts/NodeJs/Nodejs_modules/fs_module/bash/variables.bash
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+echo "this is for testing variables in bash"
+
+  #creating variables in bash 
+   name="coder0x"
+
+
+  ## how to access or print the values of variables 
+  echo $name
+  echo " "
+
+num=10
+echo "this is num $num"
+
+
+
+
