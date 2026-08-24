d1d53c3aa8b060a00f00120e4b822857845b1e33
coderx-shamsher <shamsher000789@gmail.com>
2026-08-24 18:27:06 +0530

    app and main jsx (index and app css file also)

---
+++ Frontend_library/React_Projects/React_Form_Handle_Practices/Form_Practice01/Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Form from "./Components/Form/Form";
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <section className="form_section">
+        <Form/>
+      
+      </section>
+    </>
+  );
+}
+
+export default App;
