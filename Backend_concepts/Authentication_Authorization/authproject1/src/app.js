cd155330feabe0b6a4e61b33e84d01be85fd1206
coderx-shamsher <shamsher000789@gmail.com>
2026-09-03 16:41:32 +0530

    adding cookie parser

--- Backend_concepts/Authentication_Authorization/authproject1/src/app.js
+++ Backend_concepts/Authentication_Authorization/authproject1/src/app.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import morgan from "morgan"
 import authrouter from './Routes/auth.routes.js'
-
+import cookieParser from 'cookie-parser'
 const app = express() 
 
 
 app.use(express.json())
 app.use(morgan("dev"))
-// app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth/', authrouter)
 
