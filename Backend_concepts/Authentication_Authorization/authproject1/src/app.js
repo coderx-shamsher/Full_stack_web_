219ebf96b90b4474da6a1480698b8ed56711f1b1
coderx-shamsher <shamsher000789@gmail.com>
2026-09-03 11:52:34 +0530

    app code express

---
+++ Backend_concepts/Authentication_Authorization/authproject1/src/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import morgan from "morgan"
+import authrouter from './Routes/auth.routes.js'
+
+const app = express() 
+
+
+app.use(express.json())
+app.use(morgan("dev"))
+// app.use(express.json())
+
+app.use('/api/auth/', authrouter)
+
+
+export default app
