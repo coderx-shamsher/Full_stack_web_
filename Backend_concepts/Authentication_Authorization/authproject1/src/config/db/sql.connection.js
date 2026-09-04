d14f0d90d5b1a0c4d17d796aaf50e6c6ae6e4a46
coderx-shamsher <shamsher000789@gmail.com>
2026-09-04 21:06:18 +0530

    create session fnc

--- Backend_concepts/Authentication_Authorization/authproject1/src/config/db/sql.connection.js
+++ Backend_concepts/Authentication_Authorization/authproject1/src/config/db/sql.connection.js
@@ -29,9 +29,36 @@ async function connectToDb() {
   }
 }
 
+async function session() {
+  //  let user = await pool.query('select id, name, email from users')
+
+  //  console.log(user[0][])
+
+  let sessionqy = `
+  CREATE TABLE session_users(
+      user_session_id  serial not null, 
+      id INT,
+      RefreshTkHash VARCHAR(400) NOT NULL,
+      Ip VARCHAR(100) NOT NULL,
+      User_Agent VARCHAR(100) NOT NULL,
+      login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      Revoked BOOLEAN DEFAULT false ,
+      FOREIGN KEY (id) references users(id) on delete cascade  
+  );
+  `
+ 
+  // creating table mysql query --->> 
+//  let sess = await pool.query(sessionqy)
+//  console.log(sess)
+ 
+// retreiving data mysql query ---> 
+ let user = await pool.query('select * from session_users')
+  console.log(user[0])
+}
+
 connectToDb();
 // show()
 // users()
-
+session()
 
 export default pool;
