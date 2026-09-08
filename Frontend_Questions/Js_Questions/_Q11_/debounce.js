2a1d4bf7e2ff9e49c7e0105d8f59909bb56aa060
coderx-shamsher <shamsher000789@gmail.com>
2026-09-08 13:20:22 +0530

    debounce js code

---
+++ Frontend_Questions/Js_Questions/_Q11_/debounce.js
@@ -0,0 +1,49 @@
+// Debouncing – “wait till activity stops”
+// Idea:
+// Jab events bahut tezi se aa rahe hon (jaise typing), toh har event pe function mat chalao.
+// Instead, har event pe timer reset karo, aur jab events ruk jayein for delay ms, tab function chalao. 
+
+
+// Use cases: 
+// Search input (API call sirf jab user type karna band kare).
+// Window resize (layout calculation sirf resize end pe).
+// Button double-click prevention.
+
+
+function Debouncing(fnc,delay){
+    let timer ;
+
+    return (...arg)=>{
+         clearTimeout(timer)
+
+        timer = setTimeout(()=>{
+            fnc(...arg)
+        }, delay)
+    }
+}
+
+
+function debounce(fn, delay) {
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId); // purana timer cancel
+    timeoutId = setTimeout(() => {
+    //   fn.apply(this, args);  // original function call
+    }, delay);
+  };
+}
+
+function handleSearch(e) {
+  console.log('Search for:', e.target.value);
+  // API call yahan
+}
+
+// let resultshow = () =>{
+//     console.log("Wait searching results in db.....")
+// }
+
+let input = document.querySelector("#search")
+
+// input.addEventListener("input",Debouncing(resultshow,400))
+input.addEventListener("input",debounce(handleSearch,400))
