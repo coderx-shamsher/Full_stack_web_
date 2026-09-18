15a9fea1014482424c0ba00e10be39a98815fda6
coderx-shamsher <shamsher000789@gmail.com>
2026-09-18 21:53:21 +0530

    app tsx code

---
+++ Frontend_library/Gsap_library/_code_08/src/App.tsx
@@ -0,0 +1,18 @@
+
+// import { useGSAP } from '@gsap/react'
+import './App.css'
+
+import GsapFrom from './components/GsapFrom'
+import GsapFromTo from './components/GsapFromTo'
+// import GsapTo from './components/GsapTo'
+// import gsap from 'gsap'
+
+function App() {
+  return (
+    //  <GsapTo/>
+    //  <GsapFrom/>
+    <GsapFromTo/>
+  
+)}
+
+export default App
