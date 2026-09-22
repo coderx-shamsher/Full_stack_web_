9ced98074b35257b1b814fd76886fd9fb5062db2
coderx-shamsher <shamsher000789@gmail.com>
2026-09-22 21:25:03 +0530

    sql functions

---
+++ Database/MySQL/functions/functions.sql
@@ -0,0 +1,18 @@
+
+SHOW DATABASES;  
+
+
+use testdb; 
+
+select * from testusers;   
+
+
+select count(*) from testusers;   
+
+
+select count(*) from testusers where status='inactive';
+
+select * from testusers where status='inactive'; 
+
+-- as 
+
