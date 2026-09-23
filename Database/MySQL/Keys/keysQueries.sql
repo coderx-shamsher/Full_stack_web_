6556d55ea3ae3878e897026c9a4aecf043bed2af
coderx-shamsher <shamsher000789@gmail.com>
2026-09-23 13:56:42 +0530

    foregin key sql queries

---
+++ Database/MySQL/Keys/keysQueries.sql
@@ -0,0 +1,19 @@
+show databases;
+
+
+show tables in testdb;
+
+USE  testdb;
+
+show tables ;
+
+
+select * from users;
+
+select  * from address;
+
+
+-- how to drop foregin key
+# alter table address
+# drop  foreign key fkey_userid; -- this is how to drop foregin key with alter table
+
