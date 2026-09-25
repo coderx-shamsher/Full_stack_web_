2d0cbcb7ce26e88cedd67d649c90c898cfac10df
coderx-shamsher <shamsher000789@gmail.com>
2026-09-25 17:59:22 +0530

    Home page / page of app

---
+++ PracticeProjects/Users-REST-APIs/Frontend/src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import HomeNavbar from "../components/HomeNavbar"
+import './Home.styles.css'
+
+const Home = () => {
+  return (
+    <>
+    <div className="PageSection1">
+     <HomeNavbar/> 
+
+    </div>
+    <div className="speration">
+      
+    </div>
+    <div className="PageSection2">
+         
+    </div>
+    </>
+  )
+}
+
+export default Home
