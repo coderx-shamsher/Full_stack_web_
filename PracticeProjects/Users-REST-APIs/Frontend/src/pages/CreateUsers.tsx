2de655344185e726f83d92b59eb88a440c55511b
coderx-shamsher <shamsher000789@gmail.com>
2026-09-26 15:23:49 +0530

    createuser page update

--- PracticeProjects/Users-REST-APIs/Frontend/src/pages/CreateUsers.tsx
+++ PracticeProjects/Users-REST-APIs/Frontend/src/pages/CreateUsers.tsx
@@ -1,10 +1,15 @@
+import CreateUserForm from "../components/CreateUserForm"
 import HomeNavbar from "../components/HomeNavbar"
 
 
 const CreateUsers = () => {
   return (
     <>
+    <section className="create_user_main_container">
+
       <HomeNavbar/>
+      <CreateUserForm/>
+    </section>
     </>
   )
 }
