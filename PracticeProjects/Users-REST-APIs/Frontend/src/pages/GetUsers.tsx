94c56dbc3e8dd6e5a16bce2c34029130e6b93322
coderx-shamsher <shamsher000789@gmail.com>
2026-09-26 15:25:06 +0530

    get users page updated

--- PracticeProjects/Users-REST-APIs/Frontend/src/pages/GetUsers.tsx
+++ PracticeProjects/Users-REST-APIs/Frontend/src/pages/GetUsers.tsx
@@ -1,11 +1,30 @@
-import HomeNavbar from "../components/HomeNavbar"
+
+import GetUsersCard from "../components/GetUsersCard";
+import HomeNavbar from "../components/HomeNavbar";
 
 const GetUsers = () => {
+ 
+
   return (
     <>
-     <HomeNavbar/>
+      <HomeNavbar />
+      <section
+        className="Cards"
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundColor: "rgba(77, 77, 61, 0.46)",
+          display: "flex",
+          gap : "10px",
+          flexDirection : "column",
+          paddingLeft : "10px",
+          flexWrap : "wrap"
+        }}
+      >
+        <GetUsersCard />
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default GetUsers
+export default GetUsers;
